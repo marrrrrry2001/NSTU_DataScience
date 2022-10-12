6519407caa5e6384c23db6a9c0ae86e58116c6f9
maria <masa_dor@mail.ru>
2022-10-12 22:14:46 +0700

    Написала более красиво.

--- task1.py
+++ task1.py
@@ -9,27 +9,23 @@ import time
 
 # пользователь вводит число
 x = float(input())
-# добавила вычисление корня и логарифма из библиотек math и cmath с помощью анонимной функции
-math_sqrt, math_log = lambda x: math.sqrt(x), lambda x: math.log(x)
-cmath_sqrt, cmath_log = lambda x: cmath.sqrt(x), lambda x: cmath.log(x)
+# число итераций
+count = 1000000
 
-# время начала и конца выполнения
-time1 = time.time()
-for __ in range(1000000):
-    math_sqrt(x)
-time2 = time.time()
-for __ in range(1000000):
-    math_log(x)
-time3 = time.time()
-for __ in range(1000000):
-    cmath_sqrt(x)
-time4 = time.time()
-for __ in range(1000000):
-    cmath_log(x)
-time5 = time.time()
 
-# вывод в консоль
-print('time math_sqrt after 1000000 iterations: ', abs(time1 - time2))
-print('time math_log after 1000000 iterations:  ', abs(time2 - time3))
-print('time cmath_sqrt after 1000000 iterations:', abs(time3 - time4))
-print('time cmath_log after 1000000 iterations: ', abs(time4 - time5))
+def print_time(f, count, x):
+    time1 = time.time()
+    list(map(lambda y: f(x), range(count)))
+    time2 = time.time()
+    return time2 - time1
+
+
+funcs = (
+    'math.sqrt',
+    'math.log',
+    'cmath.sqrt',
+    'cmath.log',
+)
+
+for f in funcs:
+    print(f, print_time(eval(f), count, x))
